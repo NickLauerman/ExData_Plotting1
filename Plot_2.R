9fb0deef59ed5b7aab434331d0ce4e952c8d2da5
Nick Lauerman <NL4coursera@gmail.com>
2014-06-02 19:06:19 -0500

    plot 2 completed

--- Plot_2.R
+++ Plot_2.R
@@ -1,14 +1,21 @@
 # Plot 2
 # Time series of Global Active Power 
 # 
+library(lubridate)
 file = "household_power_consumption.txt"
 data <- read.table(file = file,
                    header = TRUE,
                    sep = ";",
                    na.strings = "?",
                    stringsAsFactors = FALSE)
+data$newDate  <-  as.POSIXct(paste(data$Date, data$Time), 
+                          format = "%d/%m/%Y %H:%M:%S")
+data$Date <- as.Date (data$Date, "%d/%m/%Y")
+work <- subset(data, Date >= "2007-02-01" & Date <= "2007-02-02")
 
 
 #wrap in code to produce the png
-paste(Date, Time)
-as.POSIXlt(z, format = "%d/%m/%Y %H:%M:%S")
+
+plot(work$newDate, work$Global_active_power, 
+     type = "l",
+     ylab = "Global Active Power (kilowatts)")
